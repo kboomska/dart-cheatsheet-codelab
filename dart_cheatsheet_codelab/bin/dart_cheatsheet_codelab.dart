2b96bef5f6d5194f6683d5414d75ef64f8cc8f48
Aleksey Kuzmin <kboomska@gmail.com>
2023-03-28 20:06:36 +0300

    Dart Cheatsheet. Exceptions

--- dart_cheatsheet_codelab/bin/dart_cheatsheet_codelab.dart
+++ dart_cheatsheet_codelab/bin/dart_cheatsheet_codelab.dart
@@ -170,6 +170,7 @@ void main() {
 /// *******************************************************************************************
 /// Named parameters
 
+/*
 class MyDataObject {
   final int anInt;
   final String aString;
@@ -219,4 +220,36 @@ void main() {
   print(anotherObj.anInt); // 1
   print(anotherObj.aString); // Another!
   print(anotherObj.aDouble); // 2.0
+}
+*/
+
+/// *******************************************************************************************
+/// Exceptions
+
+typedef VoidFunction = void Function();
+
+class ExceptionWithMessage {
+  final String message;
+  const ExceptionWithMessage(this.message);
+}
+
+// Call logException to log an exception, and doneLogging when finished.
+abstract class Logger {
+  void logException(Type t, [String? msg]);
+  void doneLogging();
+}
+
+void tryFunction(VoidFunction untrustworthy, Logger logger) {
+  // Invoking this method might cause an exception. Catch and handle
+  // them using try-on-catch-finally.
+  // untrustworthy();
+  try {
+    untrustworthy();
+  } on ExceptionWithMessage catch (e) {
+    logger.logException(e.runtimeType, e.message);
+  } on Exception catch (e) {
+    logger.logException(e.runtimeType);
+  } finally {
+    logger.doneLogging();
+  }
 }
