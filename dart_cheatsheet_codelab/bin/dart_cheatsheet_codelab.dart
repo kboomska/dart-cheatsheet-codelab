87b3087cedd38ee93be4024a88874e0e54ffaef3
Aleksey Kuzmin <kboomska@gmail.com>
2023-03-29 10:53:04 +0300

    Dart Cheatsheet. Factory constructors

--- dart_cheatsheet_codelab/bin/dart_cheatsheet_codelab.dart
+++ dart_cheatsheet_codelab/bin/dart_cheatsheet_codelab.dart
@@ -289,6 +289,7 @@ class FirstTwoLetters {
 /// *******************************************************************************************
 /// Named constructors
 
+/*
 class Color {
   int red;
   int green;
@@ -301,4 +302,39 @@ class Color {
       : red = 0,
         green = 0,
         blue = 0;
+}
+*/
+
+/// *******************************************************************************************
+/// Factory constructors
+
+class IntegerHolder {
+  IntegerHolder();
+
+  // Implement this factory constructor.
+  factory IntegerHolder.fromList(List<int> list) {
+    if (list.length == 1) return IntegerSingle(list[0]);
+    if (list.length == 2) return IntegerDouble(list[0], list[1]);
+    if (list.length == 3) return IntegerTriple(list[0], list[1], list[2]);
+
+    throw Error();
+  }
+}
+
+class IntegerSingle extends IntegerHolder {
+  final int a;
+  IntegerSingle(this.a);
+}
+
+class IntegerDouble extends IntegerHolder {
+  final int a;
+  final int b;
+  IntegerDouble(this.a, this.b);
+}
+
+class IntegerTriple extends IntegerHolder {
+  final int a;
+  final int b;
+  final int c;
+  IntegerTriple(this.a, this.b, this.c);
 }
